cb3218ce8d0e64c74634056b8c591e0dcfc7b9b8
h0bby1 <contact@iadix.com>
2016-11-24 15:43:25 +0100

    update block explorer

--- block_explorer/block_explorer.c
+++ block_explorer/block_explorer.c
@@ -576,18 +576,36 @@ OS_API_C_FUNC(int) txs(const char *params, const struct http_req *req, mem_zone_
 	else
 	{
 		char			chash[65], prm[65];
-		mem_zone_ref	block_index_node = { PTR_NULL };
+		mem_zone_ref	time_index_node = { PTR_NULL }, block_index_node = { PTR_NULL };
 		uint64_t		nblks;
 		unsigned int	tidx;
-
+		ctime_t			time;
+		unsigned int	 block_time;
 
 		tree_manager_get_child_value_i64(&my_node, NODE_HASH("block height"), &nblks);
 		tree_manager_find_child_node(&my_node, NODE_HASH("block index"), NODE_BITCORE_HASH, &block_index_node);
-	
+		tree_manager_find_child_node(&my_node, NODE_HASH("block time"), NODE_GFX_INT, &time_index_node);
+		idx = nblks;
+		if ((hdr = find_key(req->query_vars, "BlockDate")) != PTR_NULL)
+		{
+			time = parseDate(hdr->value.str);
+			block_time = 0xFFFFFFFF;
+			while ((block_time > (time + 24 * 3600)) && (idx > 1))
+			{
+				if (!tree_mamanger_get_node_dword(&time_index_node, (--idx) * 4, &block_time))
+					break;
+			}
+			if (idx <= 1)
+			{
+				release_zone_ref(&block_index_node);
+				release_zone_ref(&time_index_node);
+				return 1;
+			}
+		}
 
 		tidx = 0;
 		cur = 0;
-		idx = nblks;
+		
 		while ((cur < limit) && ((idx--)>1))
 		{
 			int ntx;
