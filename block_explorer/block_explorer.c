e0489658906f75d59fbe325b2b0313fdbe3e8e04
h0bby1 <contact@iadix.com>
2016-11-24 22:12:11 +0100

    update block explorer

--- block_explorer/block_explorer.c
+++ block_explorer/block_explorer.c
@@ -656,6 +656,9 @@ OS_API_C_FUNC(int) txs(const char *params, const struct http_req *req, mem_zone_
 			else
 				tidx += ntx;
 
+			if ((time != 0) && (cur>=limit))
+				break;
+
 			idx--;
 
 			if (!tree_mamanger_get_node_dword(&time_index_node, idx * 4, &block_time))
